77957c37126f7961f1707a18051b394fdf001201
mitva-panchal-2005 <mittupanchal842@gmail.com>
2025-08-21 12:15:24 +0530

    Add files via upload

---
+++ python assignment/practical/pra3.py
@@ -0,0 +1,8 @@
+first_name = "panchal"      
+last_name = "mitva"       
+age = 19                   
+def display_user_info():
+    print("User Information:")
+    print(f"Name: {first_name} {last_name}")
+    print(f"Age: {age} years old")
+display_user_info()
